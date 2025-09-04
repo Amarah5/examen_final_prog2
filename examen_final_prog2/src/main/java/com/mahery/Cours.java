2c7140fa0503f898fa6acb10af460fbef6bdf6c4
Amarah5 <mahery.razafimanantsoa@student.passerellesnumeriques.org>
2025-09-04 08:31:00 +0300

    feat(cours): add lombok annotations

--- examen_final_prog2/src/main/java/com/mahery/Cours.java
+++ examen_final_prog2/src/main/java/com/mahery/Cours.java
@@ -1,5 +1,12 @@
 package com.mahery;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Cours {
   private final int id;
   private String labelOfcourse;
